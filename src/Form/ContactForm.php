6bf6510db7a69d9d0c6d10e8400cb319805693a1
Ales Komarek <ales-komarek@seznam.cz>
2025-06-24 16:49:04 +0200

    feat: Form Constrains

--- src/Form/ContactForm.php
+++ src/Form/ContactForm.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ContactForm extends AbstractType
 {
@@ -13,11 +15,36 @@ class ContactForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('phone')
-            ->add('email')
-            ->add('note');
+            ->add('name', null, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['max' => 255]),
+                ],
+            ])
+            ->add('surname', null, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['max' => 255]),
+                ],
+            ])
+            ->add('phone', null, [
+                'constraints' => [
+                    new Assert\Length(['max' => 50]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Email(),
+                    new Assert\Length(['max' => 255]),
+                ],
+            ])
+            ->add('note', null, [
+                'required' => false,
+                'constraints' => [
+                    new Assert\Length(['max' => 5000]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
